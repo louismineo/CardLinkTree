b53db0215b0170f7e9093fa719b09b1fe9751dfd
louismin2 <50332624+louismin2@users.noreply.github.com>
2020-10-26 09:12:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -74,7 +74,7 @@
     <h6>Made With <span class="love">♥</span> in West, Singapore</h6>
     <h6>
       Proudly Hosted By
-      <a href="/" class="name" target="_blank"> HEROKU </a>
+      <a href="www.heroku.com" class="name" target="_blank"> HEROKU </a>
       <a id="email" href="mailto:louismineo@gmail.com"> <i class="fa fa-envelope"> </i> </a>
     </h6>
   </div>
